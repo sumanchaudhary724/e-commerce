bba77f7ace478a601ca60b67e4294aa1348e1a6a
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-01-06 19:20:43 +1100

    footer updated

--- src/app/_components/Footer/FooterComponent/Footer.tsx
+++ src/app/_components/Footer/FooterComponent/Footer.tsx
@@ -2,13 +2,17 @@
 
 import React from 'react'
 import classes from './index.module.scss'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Footer } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
 
-const FooterComponent = () => {
+const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
+  const navItems = footer?.navItems || []
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
@@ -28,7 +32,32 @@ const FooterComponent = () => {
           ))}
         </ul>
       </Gutter>
-      <div className={classes.footer}></div>
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+            </Link>
+            <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = ''
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    {item.link.label}
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
     </footer>
   )
 }
